c96bb7910df96e154ae886e6b5e461a9b5c188b7
Geovanna Miranda <72633976+GeovannaMiranda@users.noreply.github.com>
2022-06-30 10:48:37 -0300

    Add files via upload

--- Menu/menusuperior.jsx
+++ Menu/menusuperior.jsx
@@ -1,3 +1,4 @@
+import { event } from "jquery";
 import React, { useEffect, useState } from "react";
 import logo from '../img/logotora75x72.png';
 import style from '../Menu/style.css';
@@ -9,29 +10,20 @@ function Menusuperior() {
 
     useEffect(() => {
         const nav = document.getElementById('nav')
-        nav.classList.toggle('active')
-
-
+        nav.classList.toggle('active')   
+        
     }, [])
 
-
-
-
-    function toggleMenu(value) {
+    function toggleMenu() {
         setClick(btnMobile)
-
-        
     }
-
-
-
 
     return (
         <div id="header">
             <img href="" id='logo' src={logo} type="submit"></img>
             <nav id="nav">
-                <button id="btn-mobile" onClick={toggleMenu}><span id="hamburguer"></span></button>
-                <ul id="menu" >
+                <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false" onClick={toggleMenu}><span id="hamburguer"></span></button>
+                <ul id="menu" role="menu">
                     <li><a href="">Inicio</a></li>
                     <li><a href="">Cadastro</a></li>
                     <li><a href="">Consulta QRcode</a></li>
